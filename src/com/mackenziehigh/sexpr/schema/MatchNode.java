275a30ae536e459d99bbfa6619fa17f8fdf6d155
Mackenzie High <MackenzieHigh2995@GMail.com>
2017-06-28 23:07:10 -0400

    Active Development.

--- src/com/mackenziehigh/sexpr/schema/MatchNode.java
+++ src/com/mackenziehigh/sexpr/schema/MatchNode.java
@@ -1,29 +1,74 @@
 package com.mackenziehigh.sexpr.schema;
 
 import com.mackenziehigh.sexpr.Sexpr;
-import java.util.Collection;
+import com.mackenziehigh.sexpr.schema.Schema.Rule;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.Objects;
 
 /**
  * An instance of this interfaces represents
  * the successful match of a single schema rule.
  */
-public interface MatchNode
+public final class MatchNode
 {
-    /**
-     * This is the name of the rule that successfully matched.
-     *
-     * @return the name of the rule.
-     */
-    public String rule ();
+    private final Rule rule;
+
+    private final Sexpr node;
+
+    private final List<MatchNode> children;
+
+    private final boolean success;
+
+    private final int treeSize;
 
     /**
-     * This is the Sexpr that was successfully matched.
+     * Constructor.
+     *
+     * @param rule will be the rule().
+     * @param node will be the node().
+     * @param children will be the children().
+     * @param success is true, if this object describes a successful match.
+     */
+    public MatchNode (final Rule rule,
+                      final Sexpr node,
+                      final List<MatchNode> children,
+                      final boolean success)
+    {
+        this.rule = Objects.requireNonNull(rule, "rule");
+        this.node = Objects.requireNonNull(node, "node");
+        this.children = Collections.unmodifiableList(new ArrayList(Objects.requireNonNull(children, "children")));
+        this.success = success;
+        this.treeSize = children.stream().mapToInt(x -> x.treeSize()).sum() + 1;
+    }
+
+    /**
+     * This is the the rule that this result describes.
+     *
+     * @return the creator of this object.
+     */
+    public Rule rule ()
+    {
+        return rule;
+    }
+
+    /**
+     * This is the symbolic-expression that was successfully matched.
+     *
+     * <p>
+     * Usually, this is the root of a proper sub-tree
+     * from within a larger symbolic-expression.
+     * </p>
      *
      * @return the matched value.
      */
-    public Sexpr value ();
+    public Sexpr node ()
+    {
+        return node;
+    }
 
     /**
      * If there were any rules that matched sub-parts of the value(),
@@ -31,32 +76,95 @@ public interface MatchNode
      *
      * @return an immutable collection of subordinate matches.
      */
-    public List<MatchNode> children ();
+    public List<MatchNode> children ()
+    {
+        return children;
+    }
 
     /**
-     * These are the actions that should be executed
-     * before visiting any of the subordinate matches.
+     * This method retrieves the number of matches
+     * in the match-tree rooted at this node.
      *
-     * @return an immutable collection of actions.
+     * @return the size of this match-tree.
      */
-    public Collection<Consumer<Sexpr>> before ();
-
-    /**
-     * These are the actions that should be executed
-     * after visiting any of the subordinate matches.
-     *
-     * @return an immutable collection of actions.
-     */
-    public Collection<Consumer<Sexpr>> after ();
+    public int treeSize ()
+    {
+        return treeSize;
+    }
 
     /**
      * This method executes the attached actions
      * in the proper order.
      */
-    public default void execute ()
+    public void execute ()
     {
-        before().forEach(x -> x.accept(this.value()));
+        if (isSuccess())
+        {
+            rule().before().accept(node());
+        }
+
         children().forEach(x -> x.execute());
-        after().forEach(x -> x.accept(this.value()));
+
+        if (isSuccess())
+        {
+            rule().after().accept(node());
+        }
+    }
+
+    /**
+     * This method determines whether this object
+     * represents a successful match attempt.
+     *
+     * @return true, iff the match succeeded.
+     */
+    public boolean isSuccess ()
+    {
+        return success;
+    }
+
+    /**
+     * This method searches the match-tree for the last
+     * successful match and then returns a stack
+     * representing the path from the root match-node
+     * to the last successful match-node.
+     *
+     * @return the most recent success on the top of the stack
+     * and the root match on the bottom of the stack.
+     */
+    public Deque<MatchNode> lastSuccess ()
+    {
+        Deque<MatchNode> stack = new ArrayDeque<>();
+
+        /**
+         * If this match is a leaf in the match-tree
+         * and represents a successful match,
+         * then this is the first node to push
+         * onto the stack.
+         */
+        if (isSuccess() && !children().isEmpty())
+        {
+            stack.push(this);
+            return stack;
+        }
+
+        /**
+         * Search each child match, recursively,
+         * in search of successful matches.
+         * Perform the search in reverse order
+         * in order to ensure the most-recent
+         * match is found first.
+         */
+        for (int i = children.size(); i > 0; i--)
+        {
+            stack = children.get(i).lastSuccess();
+
+            if (stack.isEmpty() == false)
+            {
+                stack.addLast(this);
+                break;
+            }
+        }
+
+        return stack;
     }
 }
--- src/com/mackenziehigh/sexpr/schema/Schema.java
+++ src/com/mackenziehigh/sexpr/schema/Schema.java
@@ -1,19 +1,166 @@
 package com.mackenziehigh.sexpr.schema;
 
 import com.mackenziehigh.sexpr.Sexpr;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.SortedMap;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
- * An instance of this interface is a pattern that describes a Sexpr.
+ * An instance of this class is a pattern that describes a symbolic-expression.
  */
-public interface Schema
+public final class Schema
 {
+    private final Schema SELF = this;
+
+    /**
+     * An instance of this class is a constraint on a single node in a symbolic-expression.
+     */
+    public final class Rule
+    {
+        private final String name;
+
+        private final BiFunction<Rule, Sexpr, MatchNode> matcher;
+
+        private final Consumer<Sexpr> before;
+
+        private final Consumer<Sexpr> after;
+
+        /**
+         * Sole Constructor.
+         *
+         * @param name will be the name().
+         * @param matcher will be the matcher().
+         * @param before will be the before().
+         * @param after will be the after().
+         */
+        public Rule (final String name,
+                     final BiFunction<Rule, Sexpr, MatchNode> matcher,
+                     final Consumer<Sexpr> before,
+                     final Consumer<Sexpr> after)
+        {
+            this.name = Objects.requireNonNull(name, "name");
+            this.matcher = Objects.requireNonNull(matcher, "matcher");
+            this.before = Objects.requireNonNull(before, "before");
+            this.after = Objects.requireNonNull(after, "after");
+        }
+
+        /**
+         * This method retrieves the name of this rule.
+         *
+         * @return the name of this rule.
+         */
+        public String name ()
+        {
+            return name;
+        }
+
+        /**
+         * This method retrieves the predicate that will
+         * be used to determine whether this rule matches.
+         *
+         * @return the requirement imposed by this rule.
+         */
+        public BiFunction<Rule, Sexpr, MatchNode> matcher ()
+        {
+            return matcher;
+        }
+
+        /**
+         * This is the action that should be executed
+         * before visiting any of the subordinate matches.
+         *
+         * @return an action.
+         */
+        public Consumer<Sexpr> before ()
+        {
+            return before;
+        }
+
+        /**
+         * This is the action that should be executed
+         * after visiting any of the subordinate matches.
+         *
+         * @return an action.
+         */
+        public Consumer<Sexpr> after ()
+        {
+            return after;
+        }
+
+        /**
+         * This method retrieves the schema that this rule is defined within.
+         *
+         * @return the enclosing schema.
+         */
+        public Schema schema ()
+        {
+            return SELF;
+        }
+
+        /**
+         * This method determines whether this rule matches
+         * a given symbolic-expression.
+         *
+         *
+         * @param node is the symbolic-expression.
+         * @return a result indicating whether the match succeeded or failed.
+         */
+        public MatchNode match (final Sexpr node)
+        {
+            return matcher().apply(this, Objects.requireNonNull(node, "node"));
+        }
+    }
+
+    private final Supplier<Rule> root;
+
+    private final Supplier<SortedMap<String, Rule>> rules;
+
+    /**
+     * Sole Constructor.
+     *
+     * @param root lazily provides the root() rule of the schema.
+     * @param rules lazily provides the rules() of the schema.
+     */
+    public Schema (final Supplier<Rule> root,
+                   final Supplier<SortedMap<String, Rule>> rules)
+    {
+        this.root = Objects.requireNonNull(root, "root");
+        this.rules = Objects.requireNonNull(rules, "rules");
+    }
+
+    /**
+     * This method retrieves the root rule of the schema.
+     *
+     * @return the root rule.
+     * @throws IllegalStateException if no root rule was defined.
+     */
+    public Rule root ()
+    {
+        return root.get();
+    }
+
+    /**
+     * This method returns all of the rules defined in this schema.
+     *
+     * @return a map that maps rule-names to rules.
+     */
+    public SortedMap<String, Rule> rules ()
+    {
+        return Collections.unmodifiableSortedMap(rules.get());
+    }
+
     /**
      * This method determines whether this schema
      * matches the given symbolic-expression.
      *
-     * @param tree is the Sexpr that may match this schema.
-     * @return the result of the match
+     * @param tree may be matched by this schema.
+     * @return the result of the match attempt.
      */
-    public MatchResult match (Sexpr tree);
-
+    public MatchNode match (final Sexpr tree)
+    {
+        return root().match(tree);
+    }
 }
