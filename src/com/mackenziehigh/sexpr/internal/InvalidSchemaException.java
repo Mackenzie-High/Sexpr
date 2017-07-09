1b746868fdf478c24af153ff497002e99a24e1f7
Mackenzie High <MackenzieHigh2995@GMail.com>
2017-07-09 01:39:24 -0400

    Active Development.

---
+++ src/com/mackenziehigh/sexpr/internal/InvalidSchemaException.java
@@ -0,0 +1,10 @@
+package com.mackenziehigh.sexpr.internal;
+
+/**
+ * This type of exception indicates a problem within a schema definition.
+ */
+public class InvalidSchemaException
+        extends RuntimeException
+{
+
+}
